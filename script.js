f5f080f1fc3a7ca91b0b0bcdafe564e98a9792dc
Kort <keirtwilliamgemperle@gmail.com>
2025-12-02 20:12:24 +0800

    understanding basic DOM

---
+++ script.js
@@ -0,0 +1,24 @@
+
+      //a form element should use submit in a evenlistener
+ const form = document.querySelector('#expense-form');
+      form.addEventListener("submit", (event) => {
+       
+        //prevent reload
+    event.preventDefault();     
+             //get the elements value
+    const name = document.querySelector('#name').value;
+    const amount = document.querySelector('#amount').value;
+    const category = document.querySelector('#items').value;
+    const date = document.querySelector('#date').value;
+
+           //creating element
+     let li = document.createElement('li');
+         li.textContent = `${name} - ${amount} - ${category} - ${date}`;
+
+     let list = document.querySelector('#expense-list');
+      
+     //appending the child
+    list.appendChild(li);
+    console.log(form);
+}); 
+
